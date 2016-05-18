4aaae5c33f8229fd0465d74c6d6b07f50cced0e3
Giang Nam <namlgiang@gmail.com>
2016-05-18 15:01:25 +0700

    zzz

--- preload-1.1.js
+++ preload-1.1.js
@@ -1,1 +1,1 @@
-a = document.querySelectorAll('iframe,#logo,.nsi[style*=\"left: 8px; top: 4px\"]');for(var i=0; i<a.length; i++) a[i].remove();render_mode=2;high_quality=0;want_quality = 0;
+a = document.querySelectorAll('iframe,#logo,.nsi[style*=\"left: 8px; top: 4px\"]');for(var i=0; i<a.length; i++) a[i].remove();render_mode=2;high_quality=0;want_quality = 0;is_ios = false;
