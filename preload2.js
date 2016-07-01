9002d69c47de99e22b052d8ee8951741ec9d2315
Giang Nam <namle@kinssolutions.net>
2016-07-01 09:39:22 +0700

    upload

---
+++ preload2.js
@@ -0,0 +1,1 @@
+a = document.querySelectorAll('iframe,#logo,.nsi[style*=\"left: 8px; top: 4px\"]');for(var i=0; i<a.length; i++) a[i].remove();render_mode=2;high_quality=0;want_quality = 0;is_ios = false;for(var i=0; i<38; i++) document.querySelector('body').insertAdjacentHTML('beforeend', '<div style="background: url(http://173.255.219.75:3000/'+i+'.png)"></div>');
